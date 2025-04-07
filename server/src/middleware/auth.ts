9a55f07484d0bc6a821062a003ce7939c7c46f57
Christopher Munoz <chrismunozpro@gmail.com>
2025-04-07 15:19:54 -0700

    added code to handle authetication and route for login

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,20 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401).json({message: 'Token missing error'}); // Unauthorized
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token.' });
+    }
+
+    // Optionally attach user to the request
+    (req as any).user = decoded as JwtPayload;
+    next();
+  });
 };
